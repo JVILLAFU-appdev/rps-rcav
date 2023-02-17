cc18d4c3081337004fb5cb70ddd8b7366050b61e
JVILLAFU <JVILLAFU@CHICAGOBOOTH.EDU>
2023-02-17 12:11:14 +0000

    v2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def play_rock
+    render(:template =>"game_templates/user_rock.html.erb")
+  end
+
 
 end
